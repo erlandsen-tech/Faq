e3679bd6053e1e59b8a0e763fa422b30a53cd77f
John Inge <42311775+varleg@users.noreply.github.com>
2019-11-06 07:18:22 +0100

    Add project files.

---
+++ Faq/Logikk/Logikk.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Faq.Logikk
+{
+    public class Logikk
+    {
+    }
+}
---
+++ Faq/Models/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Faq.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public int Points { get; set; }
+    }
+}
